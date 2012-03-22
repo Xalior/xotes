50a7c1bd53ad4f59035f8596d0345f3aa1c3cf4d
D. Rimron <darran@xalior.com>
2012-03-22 23:56:43 +0000

    JS dependancies sorted out.
    -Dx

--- media/js/notes.js
+++ media/js/notes.js
@@ -9,6 +9,47 @@ var display_success = function(msg, elem) {
   msg_div.insertAfter(elem).fadeIn('slow').animate({opacity: 1.0}, 5000).fadeOut('slow',function() { msg_div.remove(); });
 };
 
+$(document).ready(function() {
+var done = function(res, status) {
+  var txt = res.responseText;
+  var data = eval('('+txt+')');
+  if (status == "success") {
+    var newLi = $('<li><a href="'+data.url+'">'+data.title+'</a></li>');
+    $("#notes").prepend(newLi);
+    $("#title").val("");
+    $("#slug").val("");
+  }
+  else display_error(data.msg, $(".new"));
+}
+
+var create_note = function() {
+  var title = $("#title").val()
+  var slug = $("#slug").val()
+  if (title != "" && slug != "") {
+    var data = { title:title, slug:slug };
+    var args = { type:"POST", url:"/ajax_create/", data:data, complete:done };
+    $.ajax(args);
+  }
+  else {
+    display_error("Requires values for both title and slug.", $(".new"));
+  }
+  return false;
+};
+
 var toggleSettingsMenu = function() {
     alert('settings menu toggled');
 }
+
+$("#create").click(create_note);
+
+$(".settingsButton").click(toggleSettingsMenu);
+
+$("#slug").keyup(function() {
+    var slug = this.value;
+    var complete = function(res, status) {
+       if (status == "success") $("#slug").css('background-color','#A0A0FF');
+       else $("#slug").css('background-color','#FFA0A0');
+    }
+    $.ajax({type:'GET', url:'/slug_available/', data:{'slug': slug }, complete:complete});
+    });
+});
