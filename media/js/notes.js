f139158d3c26e74b51109bb4709b1ca1b82c37a2
D. Rimron <darran@xalior.com>
2012-03-22 23:36:13 +0000

    Moving Js to proper locations.
    -Dx

--- media/js/notes.js
+++ media/js/notes.js
@@ -7,4 +7,8 @@ var display_error = function(msg, elem) {
 var display_success = function(msg, elem) {
   var msg_div = $('<div class="success_msg"><p>'+msg+'</p></div>');
   msg_div.insertAfter(elem).fadeIn('slow').animate({opacity: 1.0}, 5000).fadeOut('slow',function() { msg_div.remove(); });
-}; 
+};
+
+var toggleSettingsMenu = function() {
+    alert('settings menu toggled');
+}
